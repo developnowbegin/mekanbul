c22fe6ff21737276bcdbe35b24719b4bb955d529
developnowbegin <melihsinandeveloper@gmail.com>
2022-10-20 10:09:27 +0300

    siteyi dinamik hale getirdim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa', 
+    { baslik: 'Anasayfa',
+    "sayfaBaslik": {
+        "siteAd":"MekanBul",
+        "slogan":"Civardaki Mekanlari Kesfet!"
+    },
+    "mekanlar":[
+        {
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden AVM",
+            "imkanlar":["Kahve","Cay","Kek"],
+            "mesafe":"10Km"
+        },
+        {
+            "ad":"Kahve Dunyasi",
+            "puan":"5",
+            "adres":"Iyas Park AVM",
+            "imkanlar":["Kahve","Cay","Kek"],
+            "mesafe":"8Km"
+        },
+    ] 
+    
+});
 };
 
 const mekanBilgisi=function(req, res, next) {
